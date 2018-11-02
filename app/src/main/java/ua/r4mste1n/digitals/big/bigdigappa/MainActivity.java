e73ae3fd8d0bd14d6fd65f87919dffe7a9611b07
r4mstein <r4mstein@ukr.net>
2018-11-02 08:13:30 +0200

    init commit

---
+++ app/src/main/java/ua/r4mste1n/digitals/big/bigdigappa/MainActivity.java
@@ -0,0 +1,13 @@
+package ua.r4mste1n.digitals.big.bigdigappa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/ua/r4mste1n/digitals/big/bigdigappa/root/App.java
@@ -0,0 +1,14 @@
+package ua.r4mste1n.digitals.big.bigdigappa.root;
+
+import android.app.Application;
+
+/**
+ * Created by Alex Shtain on 02.11.2018.
+ */
+public final class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
